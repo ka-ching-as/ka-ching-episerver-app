75f445b36c2c30f48c44dbd675bfbfc43f343c10
Stefan Holm Olsen <stefan@stefanolsen.com>
2020-05-15 21:42:59 +0400

    Split some model classes out into separate files.

---
+++ KachingPlugIn/Models/DimensionValue.cs
@@ -0,0 +1,10 @@
+﻿namespace KachingPlugIn.Models
+{
+    public class DimensionValue
+    {
+        public string Id { get; set; }
+        public L10nString Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Color { get; set; }
+    }
+}
