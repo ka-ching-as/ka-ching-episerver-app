312957597e7bcb3aaaae5916d2b7ebba73bc62a4
Stefan Holm Olsen <stefan@stefanolsen.com>
2020-05-16 21:05:40 +0400

    Simplify KachingPlugInController.

--- KachingPlugIn/ViewModels/PlugInViewModel.cs
+++ KachingPlugIn/ViewModels/PlugInViewModel.cs
@@ -2,12 +2,12 @@
 {
     public class PlugInViewModel
     {
+        public bool ExportSingleVariantAsProduct { get; set; }
         public string ProductsImportUrl { get; set; }
         public string TagsImportUrl { get; set; }
         public string FoldersImportUrl { get; set; }
         public bool ProductExportStartButtonDisabled { get; set; }
         public bool CategoryExportStartButtonDisabled { get; set; }
         public ProgressViewModel ProgressViewModel { get; set; }
-        public string ProgressViewLocation { get; set; }
     }
 }
