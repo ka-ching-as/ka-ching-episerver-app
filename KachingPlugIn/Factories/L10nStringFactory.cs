adfe34705e6b93800e830818319fc268d5560aa6
Stefan Holm Olsen <stefan@stefanolsen.com>
2020-05-19 12:34:05 +0400

    Simplify L10nStringFactory.

--- KachingPlugIn/Factories/L10nStringFactory.cs
+++ KachingPlugIn/Factories/L10nStringFactory.cs
@@ -1,8 +1,5 @@
 ﻿using EPiServer;
 using EPiServer.Commerce.Catalog.ContentTypes;
-using EPiServer.Core;
-using EPiServer.DataAbstraction.Internal;
-using EPiServer.Logging;
 using KachingPlugIn.Models;
 using System.Collections.Generic;
 
@@ -10,58 +7,59 @@ namespace KachingPlugIn.Factories
 {
     public class L10nStringFactory
     {
-        private readonly LanguageBranchRepository _languageBranchRepository;
         private readonly IContentLoader _contentLoader;
-        private readonly ILogger _log = LogManager.GetLogger(typeof(L10nStringFactory));
 
         public L10nStringFactory(
-            LanguageBranchRepository languageBranchRepository,
             IContentLoader contentLoader)
         {
-            _languageBranchRepository = languageBranchRepository;
             _contentLoader = contentLoader;
         }
 
         public L10nString LocalizedProductName(ProductContent product)
         {
-            return LocalizedContentDisplayName(product as IContent);
+            return LocalizedContentDisplayName(product);
         }
 
         public L10nString LocalizedVariantName(VariationContent variation)
         {
-            return LocalizedContentDisplayName(variation as IContent);
+            return LocalizedContentDisplayName(variation);
         }
 
         public L10nString LocalizedCategoryName(NodeContent category)
         {
-            return LocalizedContentDisplayName(category as IContent);
+            return LocalizedContentDisplayName(category);
         }
 
-        private L10nString LocalizedContentDisplayName(IContent content)
+        private L10nString LocalizedContentDisplayName(CatalogContentBase content)
         {
             var languageDictionary = new Dictionary<string, string>();
-            var languages = _languageBranchRepository.ListAll();
-            foreach (var language in languages)
+            var cultures = content.ExistingLanguages;
+            
+            foreach (var culture in cultures)
             {
-                var languageOption = LanguageLoaderOption.Specific(language.Culture);
-                var options = new LoaderOptions() { languageOption };
-                if (content is NodeContent)
+                string displayName = null;
+                switch (content)
                 {
-                    var c = _contentLoader.Get<NodeContent>(content.ContentLink, options);
-                    if (c != null && c.DisplayName != null && language.Culture != null && language.Culture.TwoLetterISOLanguageName != null)
+                    case NodeContent _:
                     {
-                        languageDictionary[language.Culture.TwoLetterISOLanguageName] = c.DisplayName;
+                        var c = _contentLoader.Get<NodeContent>(content.ContentLink, culture);
+                        displayName = c?.DisplayName;
+                        break;
+                    }
+                    case EntryContentBase _:
+                    {
+                        var c = _contentLoader.Get<EntryContentBase>(content.ContentLink, culture);
+                        displayName = c?.DisplayName;
+                        break;
                     }
                 }
-                else if (content is EntryContentBase)
+
+                if (displayName != null && culture != null)
                 {
-                    var c = _contentLoader.Get<EntryContentBase>(content.ContentLink, options);
-                    if (c != null && c.DisplayName != null && language.Culture != null && language.Culture.TwoLetterISOLanguageName != null)
-                    {
-                        languageDictionary[language.Culture.TwoLetterISOLanguageName] = c.DisplayName;
-                    }
+                    languageDictionary[culture.TwoLetterISOLanguageName] = displayName;
                 }
             }
+
             return new L10nString(languageDictionary);
         }
     }
