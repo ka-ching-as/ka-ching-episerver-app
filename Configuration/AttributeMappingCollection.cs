1bd3671bc99652399eb1cfe38ae8dfc118f2be44
Stefan Holm Olsen <stefan@stefanolsen.com>
2020-05-19 22:21:08 +0400

    Implement plugin configuration using XML config.

---
+++ Configuration/AttributeMappingCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+
+namespace KachingPlugIn.Configuration
+{
+    [ConfigurationCollection(
+        typeof(AttributeMappingCollection),
+        CollectionType = ConfigurationElementCollectionType.AddRemoveClearMap)]
+    public class AttributeMappingCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new AttributeMappingElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((AttributeMappingElement)element).MetaField;
+        }
+    }
+}
