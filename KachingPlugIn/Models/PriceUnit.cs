6ac9908bea66e8dbf60d8a124748c507efe62caf
Stefan Holm Olsen <stefan@stefanolsen.com>
2020-05-19 13:27:44 +0400

    Add PriceUnit enum and selection factory.

---
+++ KachingPlugIn/Models/PriceUnit.cs
@@ -0,0 +1,18 @@
+﻿namespace KachingPlugIn.KachingPlugIn.Models
+{
+    public enum PriceUnit
+    {
+        Grams,
+        Kilograms,
+        Meters,
+        Centimeters,
+        Millimeters,
+        SquareMeters,
+        SquareCentimeters,
+        SquareMillimeters,
+        Liters,
+        Deciliters,
+        Centiliters,
+        Milliliters
+    }
+}
---
+++ KachingPlugIn/Models/PriceUnitSelectionAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EPiServer.Cms.Shell.UI.ObjectEditing.EditorDescriptors.SelectionFactories;
+using EPiServer.Shell.ObjectEditing;
+
+namespace KachingPlugIn.KachingPlugIn.Models
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PriceUnitSelectionAttribute : SelectOneAttribute
+    {
+        public override Type SelectionFactoryType
+        {
+            get => typeof(LanguageSelectionFactory);
+            set => throw new NotSupportedException();
+        }
+    }
+}
