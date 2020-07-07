255f6c6aaab5d69f87a2f0cdbe7e7221867b5b25
Stefan Holm Olsen <stefan@stefanolsen.com>
2020-07-07 22:07:46 +0300

    Implement Batch extension for IEnumerable.
    (cherry picked from commit 10f9c34822b7d0022876e12954883d7e8b92f251)

---
+++ EnumerableExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace KachingPlugIn
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> collection, int batchSize)
+        {
+            List<T> nextBatch = new List<T>(batchSize);
+            foreach (T item in collection)
+            {
+                nextBatch.Add(item);
+
+                if (nextBatch.Count < batchSize)
+                {
+                    continue;
+                }
+
+                yield return nextBatch;
+
+                nextBatch = new List<T>(batchSize);
+            }
+
+            if (nextBatch.Count > 0)
+            {
+                yield return nextBatch;
+            }
+        }
+    }
+}
