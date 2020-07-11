3e19c3be07191970edb1f40de95c4cf746f95c35
Stefan Holm Olsen <stefan@stefanolsen.com>
2020-07-11 20:32:32 +0300

    Extend APIFacade to support deletions with a JSON body.

--- KachingPlugIn/Helpers/APIFacade.cs
+++ KachingPlugIn/Helpers/APIFacade.cs
@@ -11,7 +11,32 @@ namespace KachingPlugIn.Helpers
     {
         private static readonly ILogger _log = LogManager.GetLogger(typeof(APIFacade));
 
-        public static HttpStatusCode Delete(IList<string> ids, string url)
+        public static HttpStatusCode DeleteObject(object model, string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+
+            request.Method = "DELETE";
+            request.ContentType = "application/json";
+
+            using (Stream dataStream = request.GetRequestStream())
+            using (StreamWriter streamWriter = new StreamWriter(dataStream))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.ContractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = new SnakeCaseNamingStrategy()
+                };
+                serializer.NullValueHandling = NullValueHandling.Ignore;
+
+                serializer.Serialize(streamWriter, model);
+            }
+
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+            return response.StatusCode;
+        }
+
+        public static HttpStatusCode Delete(IEnumerable<string> ids, string url)
         {
             var deleteUrl = url + "&ids=" + string.Join(",", ids);
             _log.Information("Delete url: " + deleteUrl);
